6ad12cc3d3c447b9be6f654935d8358985813504
Turner Eison <turner@flippum.com>
2019-03-19 21:04:07 -0400

    Initial Commit

---
+++ Platforms/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Platforms
+//
+//  Created by Turner Eison on 3/19/19.
+//  Copyright © 2019 Turner Eison. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
